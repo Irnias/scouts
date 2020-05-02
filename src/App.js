4f5d1c70aca7148288b4458dc7c3a41a8505dd83
Irnias <ramiro.daccorso@gmail.com>
2020-05-02 13:58:13 -0300

    fix bug

--- src/App.js
+++ src/App.js
@@ -32,9 +32,13 @@ const Button = styled.button`
 let result = false;
 
 function handleSubmit(event) {
-  (result) ? alert("Correcto! 87, al dar vuelta la imagen y leer los aparcamientos bien, y 'LA' se encuentra repetido en la segunda imagen") : alert("Respuesta incorrecta!");
+  if(result){
+    alert("Correcto! 87, al dar vuelta la imagen y leer los aparcamientos bien, y 'LA' se encuentra repetido en la segunda imagen")
+    window.location.href = "https://posta7.now.sh/"
+  } else {
+    alert("Respuesta incorrecta!")
+  }
   event.preventDefault();
-  window.location.href = "https://posta7.now.sh/"
 }
 
 const useInputValue = initialValue => {
